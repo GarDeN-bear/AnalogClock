4afa4f8ff8e5387b3afdc3b276552dc397da5f94
GarDeN001 <garpinich22@mail.ru>
2023-07-31 13:34:20 +0300

    created class AnalogClock

--- main.cpp
+++ main.cpp
@@ -1,16 +1,69 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
 #include <chrono>
+#include <iostream>
 
 using namespace std::chrono_literals;
 
+class AnalogClock
+{
+public:
+    AnalogClock(const int _x, const int _y, const int _w, const int _h) : x(_x), y(_y), w(_w), h(_h)
+    {
+        if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+        {
+            std::cout << SDL_GetError() << std::endl;
+        }
+        else
+        {
+            win = SDL_CreateWindow("Analog Clock",
+                                   x, y,
+                                   w, h,
+                                   SDL_WINDOW_OPENGL);
+            if (win == nullptr)
+            {
+                std::cout << SDL_GetError() << std::endl;
+            }
+            else
+            {
+                screen = SDL_GetWindowSurface(win);
+                SDL_FillRect(screen, nullptr, SDL_MapRGB(screen->format, 0xFF, 0xFF, 0xFF));
+                SDL_UpdateWindowSurface(win);
+                bool quit = false;
+                SDL_Event e;
+                while (!quit)
+                {
+                    while (SDL_PollEvent(&e) != 0)
+                    {
+                        if (e.type == SDL_QUIT)
+                        {
+                            quit = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    ~AnalogClock()
+    {
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+    }
+
+private:
+    SDL_Window *win = nullptr;
+    SDL_Surface *screen = nullptr;
+    const int x;
+    const int y;
+    const int w;
+    const int h;
+};
+
 int main()
 {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window * win = SDL_CreateWindow("title", 0, 0, 800, 800, SDL_WINDOW_SHOWN);
-    SDL_Surface * screen = SDL_GetWindowSurface(win);
-    SDL_Delay(2000);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
+    AnalogClock w(
+        SDL_WINDOWPOS_CENTERED_MASK,
+        SDL_WINDOWPOS_CENTERED_MASK,
+        1920, 1080);
     return 0;
 }
